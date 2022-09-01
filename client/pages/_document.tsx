507eb4a494b8f7a93e87be8d67ca6a6dc06354e2
realet <lerealet@gmail.com>
2022-09-01 02:49:01 +0200

    whoops ep.2

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="shortcut icon" href="logosquare-128x128.png" type="image/x-icon"/>
+        <link rel="shortcut icon" href="/logosquare-128x128.png" type="image/x-icon"/>
       </Head>
       <body>
         <Main />
